cff8906e078ec7008017dc3f17579136c9834b6c
Zhibek <thezhibeks@gmail.com>
2024-02-08 19:28:43 +0600

    commented

--- portfolio-react/redux/src/App.jsx
+++ portfolio-react/redux/src/App.jsx
@@ -1,19 +1,20 @@
 import "./index.css";
-import Home from "./pages/Home";
-import AboutMe from "./pages/AboutMe";
-import BudgetDivision from "./pages/BudgetDivision";
-import CheckWeather from "./pages/CheckWeather";
-import Games from "./pages/Games";
-import QuoteOfTheDay from "./pages/QuoteOfTheDay";
-import ForMenu from "./components/ForMenu";
-import Registration from "./components/Registration";
-import Login from "./components/Login";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Home from "./pages/Home";
+// import AboutMe from "./pages/AboutMe";
+// import BudgetDivision from "./pages/BudgetDivision";
+// import CheckWeather from "./pages/CheckWeather";
+// import Games from "./pages/Games";
+// import QuoteOfTheDay from "./pages/QuoteOfTheDay";
+// import ForMenu from "./components/ForMenu";
+// import Registration from "./components/Registration";
+// import Login from "./components/Login";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Router>
+    <h1>HELLO</h1>
+      {/* <Router>
         <ForMenu />
         <Routes>
           <>
@@ -28,7 +29,7 @@ function App() {
             <Route path="/login" element={<Login />} />
           </>
         </Routes>
-      </Router>
+      </Router> */}
     </>
   );
 }
--- portfolio-react/redux/src/components/ForMenu.jsx
+++ portfolio-react/redux/src/components/ForMenu.jsx
@@ -1,98 +1,70 @@
-import "../index.css";
-import React from "react";
-import { Menu } from "antd";
-import { Link } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { setLoggedOut } from "../reducers/LoginReducer";
+// import "../index.css";
+// import { Menu } from "antd";
+// import { Link } from "react-router-dom";
 
-function LogoutLink() {
-  const dispatch = useDispatch();
+// function getItem(label) {
+//   return {
+//     label,
+//   };
+// }
+// const itemsFive = [
+//   getItem(
+//     <Link className="linkStyle" to="/homePage">
+//       🏠Home
+//     </Link>
+//   ),
+//   getItem(
+//     <Link className="linkStyle" to="/aboutMe">
+//       aboutMe
+//     </Link>
+//   ),
+//   getItem(
+//     <Link className="linkStyle" to="/budgetDivision">
+//       budgetDivision
+//     </Link>
+//   ),
+//   getItem(
+//     <Link className="linkStyle" to="/checkWeather">
+//       checkWeather
+//     </Link>
+//   ),
+//   getItem(
+//     <Link className="linkStyle" to="/games">
+//       games
+//     </Link>
+//   ),
+//   getItem(
+//     <Link className="linkStyle" to="/quoteOfTheDay">
+//       quoteOfTheDay
+//     </Link>
+//   ),
+//   getItem(
+//     <Link className="linkStyle" to="/registration">
+//       Registration
+//     </Link>
+//   ),
+//   getItem(
+//     <Link className="linkStyle" to="/login">
+//       Login
+//     </Link>
+//   ),
+// ];
 
-  const onLogout = () => {
-    dispatch(setLoggedOut(true));
+// const ForMenu = () => {
+//   return (
+//     <>
+//       <div className="forMenu">
+//         <Menu
+//           className="menuStyle"
+//           defaultSelectedKeys={["1"]}
+//           defaultOpenKeys={["sub1"]}
+//           mode="horizontal"
+//           theme="none"
+//           items={itemsFive}
+//         />
+//       </div>
+//     </>
+//   );
+// };
 
-    localStorage.removeItem("loggedIn");
-    setTimeout(() => {
-      window.location.href = "/login";
-    }, 1000);
-  };
-  return (
-    <Link className="linkStyle" onClick={onLogout}>
-      Log out
-    </Link>
-  );
-}
-
-function getItem(label) {
-  return {
-    label,
-  };
-}
-const itemsFive = [
-  getItem(
-    <Link className="linkStyle" to="/homePage">
-      🏠Home
-    </Link>
-  ),
-  getItem(
-    <Link className="linkStyle" to="/todoList">
-      📝To Do List
-    </Link>
-  ),
-  getItem(
-    <Link className="linkStyle" to="/listItem">
-      LIST Item
-    </Link>
-  ),
-  getItem(
-    <Link className="linkStyle" to="/password">
-      🔐Input Password
-    </Link>
-  ),
-  getItem(
-    <Link className="linkStyle" to="/notification">
-      📳Notification
-    </Link>
-  ),
-  getItem(
-    <Link className="linkStyle" to="/colorChange">
-      🔄Color Change
-    </Link>
-  ),
-  getItem(<LogoutLink />),
-];
-const itemsTwo = [
-  getItem(
-    <Link className="linkStyle" to="/registration">
-      Registration
-    </Link>
-  ),
-  getItem(
-    <Link className="linkStyle" to="/login">
-      Login
-    </Link>
-  ),
-];
-
-const ForMenu = () => {
-  const loginStorage = JSON.parse(localStorage.getItem("loggedIn"));
-  // console.log("forMenu", loginStorage);
-  return (
-    <>
-      {loginStorage ? (
-        <div className="forMenu">
-          <Menu
-            className="menuStyle"
-            defaultSelectedKeys={["1"]}
-            defaultOpenKeys={["sub1"]}
-            mode="horizontal"
-            theme="none"
-            items={loginStorage ? itemsFive : itemsTwo}
-          />
-        </div>
-      ) : null}
-    </>
-  );
-};
-
-export default ForMenu;
+// export default ForMenu;
