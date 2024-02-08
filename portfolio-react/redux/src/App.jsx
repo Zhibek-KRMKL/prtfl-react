526ec248d0d22cef368be461c0399772fb62f59b
Zhibek <thezhibeks@gmail.com>
2024-02-08 16:16:53 +0600

    added Router for menu

--- portfolio-react/redux/src/App.jsx
+++ portfolio-react/redux/src/App.jsx
@@ -1,26 +1,34 @@
 import "./index.css";
 import Home from "./pages/Home";
 import AboutMe from "./pages/AboutMe";
-import BudgetDivision from "./pages/BudgetDivision"
-import CheckWeather from "./pages/CheckWeather"
-import Games from "./pages/Games"
-import QuoteOfTheDay from "./pages/QuoteOfTheDay"
-import ForMenu from "./components/ForMenu"
-import Registration from "./components/Registration"
-import Login from "./components/Login"
+import BudgetDivision from "./pages/BudgetDivision";
+import CheckWeather from "./pages/CheckWeather";
+import Games from "./pages/Games";
+import QuoteOfTheDay from "./pages/QuoteOfTheDay";
+import ForMenu from "./components/ForMenu";
+import Registration from "./components/Registration";
+import Login from "./components/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Home />
-      <AboutMe/>
-      <BudgetDivision/>
-      <CheckWeather/>
-      <Games/>
-      <QuoteOfTheDay/>
-      <ForMenu/>
-      <Registration/>
-      <Login/>
+      <Router>
+        <ForMenu />
+        <Routes>
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/homePage" element={<Home />} />
+            <Route path="/aboutMe" element={<AboutMe />} />
+            <Route path="/budgetDivision" element={<BudgetDivision />} />
+            <Route path="/checkWeather" element={<CheckWeather />} />
+            <Route path="/games" element={<Games />} />
+            <Route path="/quoteOfTheDay" element={<QuoteOfTheDay />} />
+            <Route path="/registration" element={<Registration />} />
+            <Route path="/login" element={<Login />} />
+          </>
+        </Routes>
+      </Router>
     </>
   );
 }
