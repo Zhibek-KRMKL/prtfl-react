23b795da55b010bd0ad61c53aab594a46145e8a8
Zhibek <thezhibeks@gmail.com>
2024-02-08 16:01:17 +0600

    added 9 new components

--- portfolio-react/redux/src/App.jsx
+++ portfolio-react/redux/src/App.jsx
@@ -1,11 +1,28 @@
-import './App.css'
+import "./index.css";
+import Home from "./pages/Home";
+import AboutMe from "./pages/AboutMe";
+import BudgetDivision from "./pages/BudgetDivision"
+import CheckWeather from "./pages/CheckWeather"
+import Games from "./pages/Games"
+import QuoteOfTheDay from "./pages/QuoteOfTheDay"
+import ForMenu from "./components/ForMenu"
+import Registration from "./components/Registration"
+import Login from "./components/Login"
 
 function App() {
-
   return (
     <>
+      <Home />
+      <AboutMe/>
+      <BudgetDivision/>
+      <CheckWeather/>
+      <Games/>
+      <QuoteOfTheDay/>
+      <ForMenu/>
+      <Registration/>
+      <Login/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
